7d7ab970dd7a0f56dd3450b3fbd7e85c6fd36601
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-12 05:19:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,7 +14,7 @@ class Backer
   def backed_projects
     array = []
     ProjectBacker.all.collect do |project|
-      if project.backer==self
+      if project.backer == self
         array << project.project
       end
     end
