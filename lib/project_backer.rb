44f331b4c5312635c0e209b572ff050784613db4
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-12 05:20:51 +0000

    Automatically backed up by Learn

--- lib/project_backer.rb
+++ lib/project_backer.rb
@@ -1,13 +1,16 @@
 class ProjectBacker
-  attr_reader :project, :backer 
-  @@all = []
-  def initialize(project, backer)
-    @project = project
-    @backer = backer
-    @all << self
-  end
-  def self.all
-    @@all
-  end
-  
+
+    @@all = []
+    attr_reader :project, :backer
+
+    def initialize(project, backer)
+        @project = project
+        @backer = backer
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end 
+
 end
